0add95fbf1292bf9c8571a212b77d08d62b594a8
Jorge <jhiguitam@unal.edu.co>
2024-09-28 21:06:53 -0500

    Fix Perfil Heroe
    Cambios en la distribución  de los espacios de perfil de usuario

--- src/app/perfil_heroe/page.tsx
+++ src/app/perfil_heroe/page.tsx
@@ -35,57 +35,97 @@ const PerfilHeroe: React.FC = () => {
 
     return (
         <div className={styles.perfilHeroe}>
+            {/* Centro de Solicitudes */}
             <div className={styles.leftSide}>
-                <div className="request-center">
-                    <Link href="/centro-de-solicitudes">
-                        <button>Ir al Centro de Solicitudes</button>
-                    </Link>
-                </div>
-            </div>
-            <div className={styles.center}>
-                <div className="profile-info">
-                    <Image src={heroProfile.profilePicture} alt="Foto de perfil" width={100} height={100} />
-                    <h2>{heroProfile.name}</h2>
-                    <p>ID: {heroProfile.id}</p>
-                    <p>Tipo de Perfil: {heroProfile.profileType}</p>
-                    <p>{heroProfile.description}</p>
-                </div>
-                <div className="reviews">
-                    <h3>Reseñas</h3>
-                    {currentReviews.map(review => (
-                        <div key={review.id} className="review">
-                            <p>{review.text}</p>
-                            <p>Calificación: {review.rating} estrellas</p>
-                        </div>
-                    ))}
-                    <div className="pagination">
-                        {Array.from({ length: Math.ceil(reviews.length / reviewsPerPage) }, (_, index) => (
-                            <button key={index} onClick={() => setCurrentPage(index + 1)}>{index + 1}</button>
-                        ))}
+                <div className={styles.contenedorCuadro}>
+                    <div className="request-center">
+                        <Link href="/centro-de-solicitudes">
+                            <button className={buttonStyles.centroSolicitudesButton}>
+                                Ir al Centro de Solicitudes
+                            </button>
+                        </Link>
                     </div>
                 </div>
             </div>
+
+            {/* Perfil del Héroe */}
+            <div className={styles.center}>
+                <div className={styles.contenedorCuadro}>
+                        <div className={styles.profileInfo}>
+                            {/* Parte 1: Imagen de perfil */}
+                            <div className={styles.profileImage}>
+                            <Image
+                                src={heroProfile.profilePicture}
+                                alt="Foto de perfil"
+                                layout="fill" 
+                                objectFit="cover" 
+                            />
+                            </div>
+                            {/* Parte 2: Nombre y ID */}
+                            <div className={styles.profileData}>
+                                <h2>{heroProfile.name}</h2>
+                                <p>ID: {heroProfile.id}</p>
+                                <p>Tipo de Perfil: {heroProfile.profileType}</p>
+                                <p>{heroProfile.description}</p>
+                            </div>    
+                        </div>
+                </div>
+
+                {/* Reseñas */}
+                <div className={styles.contenedorCuadro}>
+                    <div className="reviews">
+                        <h3>Reseñas</h3>
+                        {currentReviews.map(review => (
+                            <div key={review.id} className="review">
+                                <p>{review.text}</p>
+                                <p>Calificación: {review.rating} estrellas</p>
+                            </div>
+                        ))}
+                        <div className="pagination">
+                            {Array.from({ length: Math.ceil(reviews.length / reviewsPerPage) }, (_, index) => (
+                                <button key={index} onClick={() => setCurrentPage(index + 1)}>{index + 1}</button>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            {/* Estadísticas y Pago */}
             <div className={styles.rightSide}>
-                <div className="balance">
-                    <h3>Saldo del Perfil</h3>
-                    <p>${heroProfile.balance}</p>
+                {/* Estadísticas del Héroe */}
+                <div className={styles.contenedorCuadro}>
+                    <div className="stats">
+                        <h3>Estadísticas del Perfil</h3>
+                        <p>Número de Estrellas: {heroProfile.stats.stars}</p>
+                        <p>Solicitudes Realizadas: {heroProfile.stats.requests}</p>
+                        <p>Calidad de Servicio: {heroProfile.stats.serviceQuality}</p>
+                        <p>Puntualidad: {heroProfile.stats.punctuality}</p>
+                    </div>
                 </div>
-                <div className="payment">
-                    <h3>Realizar Pago</h3>
-                    <input type="number" placeholder="Valor a pagar" id="payment-amount" />
-                    <input type="text" placeholder="ID del Héroe" id="hero-id" />
-                    <button className={buttonStyles.pagarButton}
-                    onClick={() => handlePayment(Number((document.getElementById('payment-amount') as HTMLInputElement).value), (document.getElementById('hero-id') as HTMLInputElement).value)}>Pagar</button>
-                </div>
-                <div className="stats">
-                    <h3>Estadísticas del Perfil</h3>
-                    <p>Número de Estrellas: {heroProfile.stats.stars}</p>
-                    <p>Solicitudes Realizadas: {heroProfile.stats.requests}</p>
-                    <p>Calidad de Servicio: {heroProfile.stats.serviceQuality}</p>
-                    <p>Puntualidad: {heroProfile.stats.punctuality}</p>
+
+                {/* Formulario de Pago */}
+                <div className={styles.contenedorCuadro}>
+                    <div className="payment">
+                        <h3>Realizar Pago</h3>
+                        <input type="number" placeholder="Valor a pagar" id="payment-amount" />
+                        <input type="text" placeholder="ID del Héroe" id="hero-id" />
+                        <button
+                            className={buttonStyles.pagarButton}
+                            onClick={() => handlePayment(
+                                Number((document.getElementById('payment-amount') as HTMLInputElement).value),
+                                (document.getElementById('hero-id') as HTMLInputElement).value
+                            )}
+                        >
+                            Pagar
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
+
+
+
+
     );
 };
 
